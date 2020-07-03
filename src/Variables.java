1539dc6a926697132c7899de706938ff9e2584da
unknown <thuyle8824@gmail.com>
2020-07-03 13:46:19 +0700

    add

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int i=10;
+        System.out.println("i = "+i);
+        float f =20.5f;
+        System.out.println("f = "+f);
+        double d=20.5;
+        System.out.println("d = "+d);
+        boolean b=true;
+        System.out.println("b = "+b);
+        char c='a';
+        System.out.println("c = "+c);
+        String s="Hà Nội";
+        System.out.println("s = "+s);
+    }
+}
